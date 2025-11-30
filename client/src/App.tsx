5ce03a9944d56d454fc00817c39fadb1a24e861a
shamugupta8259 <shamugupta8259@gmail.com>
2025-11-30 23:13:26 +0530

    fipxpp

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toasters } from "@/components/ui/toaster";
+import { Toasters } from "@/components/ui/toasters.tsx";
 import { ThemeProvider } from "@/hooks/use-theme";
 import NotFound from "@/pages/not-found";
 
